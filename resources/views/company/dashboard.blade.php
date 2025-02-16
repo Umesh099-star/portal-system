a9c6ac438af48faf2339489af12f7f5817c054af
portal system <umesh789pandit@gmail.com>
2025-02-16 12:32:04 +0545

    first commit

--- resources/views/company/dashboard.blade.php
+++ resources/views/company/dashboard.blade.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Company Dashboard</title>
+    <!-- <link rel="stylesheet" href="company.css"> -->
+    <link rel="stylesheet" href="{{ asset('css/company.css') }}">
+
 </head>
 <body>
     <h3>Company Dashboard</h3>
